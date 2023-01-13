5a7136135ad41a34759ce69951fb9434cd109b02
gdineshbabu <118524830+gdineshbabu@users.noreply.github.com>
2023-01-13 19:47:34 +0530

    duble.java
    You need to double the integer and return it.
    
    Input:
    
    2
    
    output:
    
    4

---
+++ duble.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class duble
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a=n*2;
+        System.out.println(a);
+    }
+}

0dedc2dfce1e18d11c9884c7b7d17c8870421472
gdineshbabu <118524830+gdineshbabu@users.noreply.github.com>
2023-01-13 19:31:24 +0530

    str.java
    Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".

---
+++ str.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class str
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String n=sc.nextLine();
+        System.out.println("Hello, "+n+" how are you doing today?");
+    }
+}

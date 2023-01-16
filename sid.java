f5b28803a20273a7deee84fb36fade09838410d6
gdineshbabu <118524830+gdineshbabu@users.noreply.github.com>
2023-01-16 12:58:34 +0530

    sid.java
    Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
    
    "I love you"
    "a little"
    "a lot"
    "passionately"
    "madly"
    "not at all"
    If there are more than 6 petals, you start over with "I love you" for 7 petals, "a little" for 8 petals and so on.
    
    When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
    
    Your goal in this question is to determine which phrase the girls would say at the last petal for a flower of a given number of petals. The number of petals is always greater than 0.
    
    INPUT 1:
    1
    OUTPUT  1:
    I love you
    INPUT 2:
    3
    OUTPUT 2:
    a lot

---
+++ sid.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class sid {
+    public static String howMuchILoveYou(int nb_petals) {
+    //write your logic here
+    Switch(a):
+    {
+        case 1:
+               System.out.println("I love you");
+        case 2:
+               System.out.println("a little");
+        case 3:
+               System.out.println("a lot");
+        case 4:
+               System.out.println("passionately");
+               
+        case 5:
+               System.out.println("madly");
+        case 6:
+              System.out.println("not at all");
+    }
+    }
+
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+
+}}

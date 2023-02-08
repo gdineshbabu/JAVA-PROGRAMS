00666b206d9679b8a1bcea19411d0145c56d31ca
gdineshbabu <118524830+gdineshbabu@users.noreply.github.com>
2023-02-08 15:22:23 +0530

    vector.java

---
+++ vector.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Vector<String> vec=new Vector<String>();
+        vec.add("cat");
+        vec.add("Dog");
+        vec.addElement("Lion");
+        System.out.println(vec);
+        
+    }
+}

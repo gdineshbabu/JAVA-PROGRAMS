00bd9b4cfdb9dcf485bea0c995d1f05c2e0957ed
gdineshbabu <118524830+gdineshbabu@users.noreply.github.com>
2023-02-09 16:53:39 +0530

    Array1.java
    Write a java program using ArrayList ti print the following output:
    
    ArrayList: [Java, Python, Swift]

---
+++ Array1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Array
+{
+    public static void main(String args[])
+    {
+        ArrayList<String> a=new ArrayList<String>();
+        a.add("Java");
+        a.add("Python");
+        a.add("Swift");
+        System.out.println("ArrayList: "+a);
+    }
+}

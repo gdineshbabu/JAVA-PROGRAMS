fd241283fe8eea525d7e4acbffd8ebd78f8306c6
gdineshbabu <118524830+gdineshbabu@users.noreply.github.com>
2023-02-09 16:54:40 +0530

    Array2.java
    Write a program using ArrayList to print the follwoing output:
    ArrayList: [Cat, Dog, Cow]
    Element at index 1: Dog

---
+++ Array2.java
@@ -0,0 +1,14 @@
+//code here
+import java.util.*;
+public class Array2
+{
+    public static void main(String args[])
+    {
+        ArrayList<String> a=new ArrayList<String>();
+        a.add("Cat");
+        a.add("Dog");
+        a.add("cow");
+        System.out.println("ArrayList: "+a);
+        System.out.println("Element at index 1: "+a.get(1));
+    }
+}

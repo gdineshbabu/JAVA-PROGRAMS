a79e28e1bcdd664a3fe2547e9a2d6f676ffb572b
gdineshbabu <118524830+gdineshbabu@users.noreply.github.com>
2023-02-09 16:52:28 +0530

    Array3.java
    Write a program using ArrayList to print the follwoing output:
    ArrayList: [Java, Kotlin, C++]
    Modified ArrayList: [Java, Kotlin, JavaScript]

---
+++ Array3.java
@@ -0,0 +1,17 @@
+//code here
+import java.util.*;
+public class Array3
+{
+    public static void main(String args[])
+    {
+        ArrayList<String> a= new ArrayList<String>();
+        a.add("Java");
+        a.add("Kotlin");
+        a.add("C++");
+        System.out.println("ArrayList: "+a);
+        a.set(2,"JavaScript");
+        System.out.println("Modified ArrayList: "+a);
+        
+        
+    }
+}
